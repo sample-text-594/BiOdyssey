b0a70b45da619195b759baa35622afd649b002c3
FrozenBear98 <FrozenBear98@users.noreply.github.com>
2019-11-03 01:33:12 +0100

    Ship energy

--- BiOdyssey/Assets/Scripts/Player/InPlanet/PlayerController.cs
+++ BiOdyssey/Assets/Scripts/Player/InPlanet/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour {
 
@@ -10,7 +11,19 @@ public class PlayerController : MonoBehaviour {
     public float baseElevation = 2.5f;
     public float elevationMargin = 0.5f;
 
+    public float boostSpeedMultiplier = 2;
+    public float boostDuration = 1;
+
+    public Slider[] energyBars;
+    public float energyConsumeMultiplier = 1;
+
     int layerMask;
+
+    public float energy;
+    int actualEnergyBar;
+
+    bool boostActive;
+    float boostTimeLeft;
 
     Transform player;
     float planetRadius;
@@ -20,28 +33,73 @@ public class PlayerController : MonoBehaviour {
         planetRadius = GameObject.Find("Planet").GetComponent<Planet>().shapeSettings.planetRadius;
 
         player.localPosition = new Vector3(0.0f, 0.0f, -planetRadius - baseElevation);
+
         layerMask = 1 << 8;
+
+        energy = 100;
+        actualEnergyBar = energyBars.Length - 1;
+
+        boostActive = false;
+        boostTimeLeft = 0.0f;
     }
     
     void Update() {
-        float vAxis = Input.GetAxis("Vertical");
-        float hAxis = Input.GetAxis("Horizontal");
+        if (energy > 0) {
+            float vAxis = Input.GetAxis("Vertical");
+            float hAxis = Input.GetAxis("Horizontal");
 
-        if (vAxis != 0.0f) {
-            transform.Rotate(new Vector3 (0.0f, -vAxis * movementSpeed * Time.deltaTime, 0.0f));
-        }
+            if (vAxis != 0.0f) {
+                if (boostActive) {
+                    transform.Rotate(new Vector3(0.0f, -vAxis * movementSpeed * boostSpeedMultiplier * Time.deltaTime, 0.0f));
 
-        if (hAxis != 0.0f) {
-            transform.Rotate(new Vector3(0.0f, 0.0f, -hAxis * rotationSpeed * Time.deltaTime));
-        }
-
-        RaycastHit hit;
-        if (Physics.Raycast(player.position, -player.up, out hit, baseElevation + elevationMargin, layerMask)) {
-            if (hit.distance < baseElevation) {
-                player.localPosition = new Vector3(0.0f, 0.0f, player.localPosition.z - 0.1f * verticalSpeed * Time.deltaTime);
+                    boostTimeLeft -= Time.deltaTime;
+                    if (boostTimeLeft < 0) {
+                        boostActive = false;
+                    }
+                } else {
+                    transform.Rotate(new Vector3(0.0f, -vAxis * movementSpeed * Time.deltaTime, 0.0f));
+                }
+                
             }
-        } else {
-            player.localPosition = new Vector3(0.0f, 0.0f, player.localPosition.z + 0.1f * verticalSpeed * Time.deltaTime);
+
+            if (hAxis != 0.0f) {
+                transform.Rotate(new Vector3(0.0f, 0.0f, -hAxis * rotationSpeed * Time.deltaTime));
+            }
+
+            RaycastHit hit;
+            if (Physics.Raycast(player.position, -player.up, out hit, baseElevation + elevationMargin, layerMask)) {
+                if (hit.distance < baseElevation) {
+                    player.localPosition = new Vector3(0.0f, 0.0f, player.localPosition.z - 0.1f * verticalSpeed * Time.deltaTime);
+                }
+            } else {
+                player.localPosition = new Vector3(0.0f, 0.0f, player.localPosition.z + 0.1f * verticalSpeed * Time.deltaTime);
+            }
+
+            ConsumeEnergy(0.1f * energyConsumeMultiplier * Time.deltaTime);
+
+            if (energy > 20 && Input.GetKeyDown(KeyCode.Space)) {
+                ConsumeEnergy(20);
+
+                boostActive = true;
+                boostTimeLeft = boostDuration;
+            }
         }
+    }
+
+    void ConsumeEnergy(float quantity) {
+        float quantityConverted = quantity / (100 / energyBars.Length);
+
+        if (energyBars[actualEnergyBar].value > quantityConverted) {
+            energyBars[actualEnergyBar].value -= quantityConverted;
+        } else {
+            quantityConverted -= energyBars[actualEnergyBar].value;
+            energyBars[actualEnergyBar].value = 0;
+            if (actualEnergyBar > 0) {
+                actualEnergyBar--;
+                energyBars[actualEnergyBar].value -= quantityConverted;
+            }
+        }
+
+        energy -= quantity;
     }
 }
