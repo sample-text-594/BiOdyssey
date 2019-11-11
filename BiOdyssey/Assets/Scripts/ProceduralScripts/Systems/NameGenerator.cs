fa3eeb5b7a259f91ca3369daf986db4431b0e021
Alberto Hernández <alberto.hernandez.1001@gmail.com>
2019-11-11 17:40:04 +0100

    Planet name generator script

---
+++ BiOdyssey/Assets/Scripts/ProceduralScripts/Systems/NameGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameGenerator {
+
+    int NUM_NOMBRES;
+    int NUM_NUMBERS;
+
+    public string generateName(int seed)
+    {
+        NUM_NOMBRES = 10;
+        NUM_NUMBERS = 100000;
+        Random.seed = seed;
+        string[] planetArray = {"Gliese-", "HD ", "Kepler-", "HIP ", "SN ", "Hubble-", "WR ", "R ", "PSR ", "IRAS-"};
+        string nombre = planetArray[Random.range(0, NUM_NOMBRES)];
+        return nombre + Random.range(0, NUM_NUMBERS);
+    }
+}
