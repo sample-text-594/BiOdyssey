c0adf6d03ceb66592a505c11e76fef6b8fb680a7
Alberto Hernández <alberto.hernandez.1001@gmail.com>
2019-11-25 02:02:03 +0100

    IA básica de las criaturas

---
+++ BiOdyssey/Assets/Scripts/Animals/Creature.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Creature : MonoBehaviour {
+
+    IStatesCreature state;
+
+    [HideInInspector]
+    public CreatureStateWander wanderState;
+
+    [HideInInspector]
+    public NavMeshAgent agent;
+
+    void Start() {
+        state = wanderState;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        state = state.Update(this);
+    }
+}
---
+++ BiOdyssey/Assets/Scripts/Animals/CreatureStateWander.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureStateWander : IStatesCreature {
+
+    //Atributo de velocidad.
+    public int speed = 3;
+
+    public IStatesCreature Update(Creature c) {
+
+        c.agent.speed = speed;
+
+        //Nuevo destino aleatorio.
+        int rand = Random.Range(0, 100);
+        if(rand > 79) {
+            c.agent.destination = c.transform.position + new Vector3((Random.value - 0.5f) * 80, (Random.value - 0.5f) * 80, (Random.value - 0.5f) * 80);
+        }
+        return c.wanderState;
+    }
+}
