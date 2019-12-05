fe93124acb8df06d9feb78786d975b6570ed428c
DavidDA98 <43207816+DavidDA98@users.noreply.github.com>
2019-12-05 13:46:20 +0100

    Creature color

---
+++ BiOdyssey/Assets/Scripts/Player/InPlanet/DiscoverCreature.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DiscoverCreature : MonoBehaviour {
+    public Star star;
+    public GameObject uiSystem;
+    public GoToPlanet gtp;
+
+    public GameObject uiNewCreature;
+    public TextMeshProUGUI creatureName;
+    public GameObject renameButton;
+    public GameObject skipButton;
+    public GameObject okButton;
+    public GameObject renameText;
+
+    public GameObject uiRenameCreature;
+    public TMP_InputField creatureNameInput;
+
+    bool isDiscovered;
+
+    public void Discover(string sn) {
+        //Comprobar en la base de datos si el sistema ya esta descubierto
+        isDiscovered = false;
+
+        if (isDiscovered) {
+            renameButton.SetActive(false);
+            skipButton.SetActive(false);
+            renameText.SetActive(false);
+        } else {
+            okButton.SetActive(false);
+        }
+
+        creatureName.SetText(sn);
+    }
+
+    public void Rename() {
+        uiNewCreature.SetActive(false);
+        uiRenameCreature.SetActive(true);
+    }
+
+    public void Skip() {
+        gtp.uiSystemEnabled = true;
+
+        gameObject.SetActive(false);
+        uiSystem.SetActive(true);
+    }
+
+    public void Confirm() {
+        string newName = creatureNameInput.text;
+
+        if (!newName.Equals("")) {
+            star.RenameStar(newName);
+            creatureName.SetText(newName);
+        }
+
+        uiRenameCreature.SetActive(false);
+        uiNewCreature.SetActive(true);
+
+        renameButton.SetActive(false);
+        skipButton.SetActive(false);
+        renameText.SetActive(false);
+        okButton.SetActive(true);
+    }
+}
--- BiOdyssey/Assets/Scripts/ProceduralScripts/Planets/PlanetBuilder.cs
+++ BiOdyssey/Assets/Scripts/ProceduralScripts/Planets/PlanetBuilder.cs
@@ -7,6 +7,9 @@ public class PlanetBuilder : MonoBehaviour {
     public Transform[] spawners;
     public Creature[] creatures;
 
+    public Gradient creature1Colors;
+    public Gradient creature2Colors;
+
     void Start() {
         Planet p = gameObject.AddComponent<Planet>();
 
@@ -25,34 +28,82 @@ public class PlanetBuilder : MonoBehaviour {
 
         for (int i = 0; i < spawners.Length; i++) {
             GameObject creature;
+            Color col;
 
             if (i % 2 == 0) {
                 creature = Instantiate(creatures[0].creaturePrefab);
-                Instantiate(creatures[0].part1[Random.Range(0, 5)]).transform.SetParent(creature.transform);
-                Instantiate(creatures[0].part2[Random.Range(0, 5)]).transform.SetParent(creature.transform);
-                Instantiate(creatures[0].part3[Random.Range(0, 5)]).transform.SetParent(creature.transform);
-                Instantiate(creatures[0].part4[Random.Range(0, 5)]).transform.SetParent(creature.transform);
-                Instantiate(creatures[0].part5[Random.Range(0, 5)]).transform.SetParent(creature.transform);
+
+                int rand = Random.Range(0, 5);
+                Instantiate(creatures[0].part1[rand]).transform.SetParent(creature.transform);
+                creature.name = "" + rand;
+
+                rand = Random.Range(0, 5);
+                Instantiate(creatures[0].part2[rand]).transform.SetParent(creature.transform);
+                creature.name += rand;
+
+                rand = Random.Range(0, 5);
+                Instantiate(creatures[0].part3[rand]).transform.SetParent(creature.transform);
+                creature.name += rand;
+
+                rand = Random.Range(0, 5);
+                Instantiate(creatures[0].part4[rand]).transform.SetParent(creature.transform);
+                creature.name += rand;
+
+                rand = Random.Range(0, 5);
+                Instantiate(creatures[0].part5[rand]).transform.SetParent(creature.transform);
+                creature.name += rand;
+
+                col = creature1Colors.Evaluate(Random.Range(0f, 1f));
             } else {
                 creature = Instantiate(creatures[1].creaturePrefab);
-                Instantiate(creatures[1].part1[Random.Range(0, 5)]).transform.SetParent(creature.transform);
-                Instantiate(creatures[1].part2[Random.Range(0, 5)]).transform.SetParent(creature.transform);
-                Instantiate(creatures[1].part3[Random.Range(0, 5)]).transform.SetParent(creature.transform);
-                Instantiate(creatures[1].part4[Random.Range(0, 5)]).transform.SetParent(creature.transform);
-                Instantiate(creatures[1].part5[Random.Range(0, 5)]).transform.SetParent(creature.transform);
+
+                int rand = Random.Range(0, 5);
+                Instantiate(creatures[1].part1[rand]).transform.SetParent(creature.transform);
+                creature.name = "" + rand;
+
+                rand = Random.Range(0, 5);
+                Instantiate(creatures[1].part2[rand]).transform.SetParent(creature.transform);
+                creature.name += rand;
+
+                rand = Random.Range(0, 5);
+                Instantiate(creatures[1].part3[rand]).transform.SetParent(creature.transform);
+                creature.name += rand;
+
+                rand = Random.Range(0, 5);
+                Instantiate(creatures[1].part4[rand]).transform.SetParent(creature.transform);
+                creature.name += rand;
+
+                rand = Random.Range(0, 5);
+                Instantiate(creatures[1].part5[rand]).transform.SetParent(creature.transform);
+                creature.name += rand;
+
+                col = creature2Colors.Evaluate(Random.Range(0f, 1f));
             }
 
             creature.transform.SetParent(g.transform);
             creature.transform.position = spawners[i].position;
+            if (i == 5) {
+                creature.transform.localEulerAngles = new Vector3(creature.transform.localEulerAngles.x + 180, creature.transform.localEulerAngles.y, creature.transform.localEulerAngles.z);
+            }
 
             NavMeshAgent nma = creature.GetComponent<NavMeshAgent>();
             nma.Warp(creature.transform.position);
             nma.updateUpAxis = true;
 
+            foreach (MeshRenderer m in creature.GetComponentsInChildren<MeshRenderer>()) {
+                m.material.SetColor("_BaseColor", col);
+            }
+
+            foreach (SkinnedMeshRenderer m in creature.GetComponentsInChildren<SkinnedMeshRenderer>()) {
+                m.material.SetColor("_BaseColor", col);
+            }
+
             for (int j = 0; j < Random.Range(2, 4); j++) {
                 nma = Instantiate(creature).GetComponent<NavMeshAgent>();
                 nma.Warp(nma.transform.position);
                 nma.updateUpAxis = true;
+
+                nma.transform.SetParent(g.transform);
             }
         }
     }
